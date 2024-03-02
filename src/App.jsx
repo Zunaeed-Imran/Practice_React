e3f40e6e57644b724295ee6a7e927d2bbf4ce271
imran <zunaeede@gmail.com>
2024-03-02 10:16:19 +0600

    main - making a index file jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
-import Practice1 from './component/practice1';
+import Todo_App from './component';
+
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <Practice1 />
+      <Todo_App/>
     </>
   );
 }
---
+++ src/component/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Todo_App = () => {
+  return (
+    <div>
+      hello form index file
+    </div>
+  )
+}
+
+export default Todo_App;
