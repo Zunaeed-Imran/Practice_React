c2f07d43b1b0ccf93def330c808cb52d1a87ea4f
imran <zunaeede@gmail.com>
2024-03-13 22:31:54 +0600

    main - practice not done of erroe handeling in useEffect in react.

--- src/UseEffect_Error_fetch/UseEffect_Error_fetch.jsx
+++ src/UseEffect_Error_fetch/UseEffect_Error_fetch.jsx
@@ -1,21 +1,36 @@
 import React, { useEffect, useState } from 'react';
 
 
+  // loading massege for loading in the display
+  const loadingMessage = <p>Data is loading from API....</p>
+
 
 const UseEffect_Error_fetch = () => {
-  const [todo1, setTodo1] = useState(false);
-  const [error, setError] = useState(false);
+
+
+  const [todo1, setTodo1] = useState(null);
   const [mas, setMas] = useState(true);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     setTimeout(() => {
       fetch('https://jsonplaceholder.typicode.com/todos')
-        .then(res => {
-          return res.json()
+        .then(res1 => {
+          if (!res1.ok) {
+            throw Error("Not Fetch")
+          } else {
+            return res1.json();
+          }
+          
             .then(todo1 => {
               setTodo1(todo1);
               setMas(false)
-        });
+              setError(null);
+            })
+            .catch((error) => {
+              setError(error.message);
+              setMas(false);
+            });
       });
     }, 1000)
   }, []);
@@ -28,15 +43,13 @@ const UseEffect_Error_fetch = () => {
       return <p key={todo1.id}>{todo1.title}</p>;
     });
 
-
-  // loading massege for loading in the display
-  const loadingMessage = <p>Data is loading from API....</p>
   
 
   return (
     <div>
       <h1>Todo</h1>
       {mas && loadingMessage}
+      {error && {error}}
       {todoDisplay}
     </div>
   );
