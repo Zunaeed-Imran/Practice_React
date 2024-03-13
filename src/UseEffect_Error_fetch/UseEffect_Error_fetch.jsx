0a3c9a654cb2dcef138abb8d47bfab7fb49e52cd
imran <zunaeede@gmail.com>
2024-03-13 20:19:30 +0600

    main - set time out and massege done.

--- src/UseEffect_Error_fetch/UseEffect_Error_fetch.jsx
+++ src/UseEffect_Error_fetch/UseEffect_Error_fetch.jsx
@@ -1,25 +1,43 @@
 import React, { useEffect, useState } from 'react';
 
+
+
 const UseEffect_Error_fetch = () => {
-  const [todo1, setTodo1] = useState();
+  const [todo1, setTodo1] = useState(false);
   const [error, setError] = useState(false);
-  const [mas, setMas] = useState();
+  const [mas, setMas] = useState(true);
 
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/todos')
-      .then(res => {
-      return res.json().then(todo1 => {
-        setTodo1(todo1);
+    setTimeout(() => {
+      fetch('https://jsonplaceholder.typicode.com/todos')
+        .then(res => {
+          return res.json()
+            .then(todo1 => {
+              setTodo1(todo1);
+              setMas(false)
+        });
       });
+    }, 1000)
+  }, []);
+
+
+  // display the todo massege taking in a variable.
+  const todoDisplay =
+    todo1 &&
+    todo1.map(todo1 => {
+      return <p key={todo1.id}>{todo1.title}</p>;
     });
-  });
+
+
+  // loading massege for loading in the display
+  const loadingMessage = <p>Data is loading from API....</p>
+  
 
   return (
     <div>
       <h1>Todo</h1>
-      {todo1 && todo1.map((todo1) => {
-        return <p key={todo1.id}>{todo1.title}</p>
-      })}
+      {mas && loadingMessage}
+      {todoDisplay}
     </div>
   );
 };
