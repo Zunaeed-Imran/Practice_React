befdd882e13b239bda06ac64c40c933ecce0261d
imran <zunaeede@gmail.com>
2024-03-19 17:32:03 +0600

    main - finally find the problem in practice two.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import './App.css';
 import CountryApp2 from './Country_App/CountryApp2';
 import Dynamic_Style_1 from './Dynamic_Style/Dynamic_Style_1';
+import Dynamic_style2 from './Dynamic_Style/Dynamic_style2';
 // import COuntry_App from './Country_App/COuntry_App';
 // import Custom_Hook2 from './Custom_Hook/Custom_Hook2';
 // import Custom_hook1 from './Custom_Hook/Custom_hook1';
@@ -130,7 +131,9 @@ function App() {
 
       {/* <CountryApp2/> */}
 
-      <Dynamic_Style_1/>
+      {/* <Dynamic_Style_1/> */}
+
+      <Dynamic_style2/>
     </>
   );
 }
