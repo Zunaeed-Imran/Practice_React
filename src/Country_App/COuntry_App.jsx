9530212db8bd90ba579a868eea1d9524633c27c4
imran <zunaeede@gmail.com>
2024-03-19 12:21:51 +0600

    main - done 1st practice of search country.

--- src/Country_App/COuntry_App.jsx
+++ src/Country_App/COuntry_App.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import Display_Country from './Display_Country';
+import Search1 from './Search1';
 
 
 const url = 'https://restcountries.com/v3.1/all';
@@ -44,12 +45,18 @@ const COuntry_App = () =>
   }
 
   const handleSearch = (searchValue) => {
-    
+    let value1 = searchValue.toLowerCase();
+    const newCountry1 = countries.filter((country1) => {
+      const countryName1 = country1.name.common.toLowerCase();
+      return countryName1.startsWith(value1);
+    });
+    setFilter(newCountry1);
   }
 
   return (
     <>
       <h1>Country App</h1>
+      <Search1 onSearch={handleSearch} />
       {loading && <h2>Loading...</h2>}
       {error && <h2>{error.message}</h2>}
       {countries && <Display_Country countries={filter}
