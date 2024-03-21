5e3625c040394c8bc220e386440fc069018fe7b0
imran <zunaeede@gmail.com>
2024-03-21 14:43:16 +0600

    Project_Practice - take the project feom main branch.

---
+++ src/Country_App/Country_data.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Country_data = props => {
+  const { name, flags, capital, population, area } = props.country;
+
+  const handleRemove = name => {
+    props.onRemove(name);
+  };
+
+  return (
+    <>
+      <div>
+        <img src={flags.png} alt={name.common} />
+        <h3>Name: {name.common}</h3>
+        <h3>Capital: {capital}</h3>
+        <h3>Population: {population}</h3>
+        <h3>Area: {area}</h3>
+        <button
+          onClick={() => {
+            handleRemove(name.common);
+          }}
+        >
+          Remove
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default Country_data;
