fcadb24aab943c7a1155f581cfa4aa46b47975c6
imran <zunaeede@gmail.com>
2024-03-21 14:52:56 +0600

    Practice_Project - delete all from this branch. all data only in the main branch.

--- src/App.jsx
+++ src/App.jsx
@@ -1,146 +1,20 @@
-// import { useState } from 'react';
 import './App.css';
-import Usereducer from './Hooks/Use_reducer_Hook/Usereducer';
-// import Component1 from './Hooks/UseContext_Hook/Component1';
+
 import COuntry_App from './Country_App/COuntry_App';
-// import Dynamic_Style_1 from './Dynamic_Style/Dynamic_Style_1';
-// import Dynamic_style2 from './Dynamic_Style/Dynamic_style2';
-// import Style3_dynamic from './Dynamic_Style/Style3_dynamic';
-// import COuntry_App from './Country_App/COuntry_App';
-// import Custom_Hook2 from './Custom_Hook/Custom_Hook2';
-// import Custom_hook1 from './Custom_Hook/Custom_hook1';
-// import UseEffectErrorPrac2 from './UseEffect_Error_fetch/UseEffectErrorPrac2';
-// import UseEffect_Error_fetch from './UseEffect_Error_fetch/UseEffect_Error_fetch';
-// import UseEffect3_practice from './useEffect_fetch_practice/UseEffect3_practice';
-// import UseEffect2 from './useEffect_fetch_practice/UseEffect2';
-// import Useeffect_practice1 from './useEffect_fetch_practice/Useeffect_practice1';
-// import Faq3_practice from './FAQ_practice/Faq3_practice';
-// import Faq2_Practice from './FAQ_practice/Faq2_Practice';
-// import FAQ_Practice from './FAQ_practice/FAQ_Practice';
-// import Practice_toggle2 from './Toggle_practice/Practice_toggle2';
-// import Toggle_Practice from './Toggle_practice/Toggle_Practice';
-// import Todo_App from './Todo_app/Todo_App';
-// import Practice3_Home from './State_lifting_principle/Practice3_Home';
-// import Practice2Home from './State_lifting_principle/Practice2Home';
-// import Home from './State_lifting_principle/Home';
-// import State_lifting from './More_on_state_lifting/State_lifting';
-// import Parent5 from './Passing_data_child_to_parent/Parent5';
-// import Parent4 from './Passing_data_child_to_parent/Parent4';
-// import Parent3 from './Passing_data_child_to_parent/Practice3';
-// import Child_to_parent from './Passing_data_child_to_parent/Child_to_parent';
-// import Passing_data_child_to_parent from './Passing_data_child_to_parent/Passing_data_child_to_parent';
-// import Practice3 from './UseState_with_Object/Practice3';
-// import Practice2 from './UseState_with_Object/Practice2';
-// import Practice1 from './UseState_with_Object/Practice1';
-// import Form_data_show from './Controlled_component_Collect_form_data./Form_data_show';
-// import Practice1 from './Controlled_component_Collect_form_data./Practice1';
-// import EventHandle_onchange3 from './Event_handler_onchange_onclick/EventHandle_onchange3';
-// import EventHandler2 from './Event_handler_onchange_onclick/eventHandler2';
-// import Event_handler from './Event_handler_onchange_onclick/Event_handler';
-// import Event_handler from './Event_handler_onchange_onclick/eventHandler2';
-// import Event_handler from './Event_handler_onchange_onclick/Event_handler';
-// import Usestatehook3 from './useState_hook/Usestatehook3';
-// import UseStateHook2 from './useState_hook/UseStateHook2';
-// import Usestate_hook from './useState_hook/Usestate_hook';
-// import Mapping from './mapping_practice/Mapping';
-// import State3 from './state_setState_eventHandler/State3';
-// import Prop from './Prop_practice/Prop';
-// import State from './state_setState_eventHandler';
 // import Todo_App from './component';
 
 
 function App() {
-  // const [count, setCount] = useState(0);
 
   return (
     <>
       {/* <Todo_App/> */}
 
-      {/* <State/> */}
-
-      {/* <State3 /> */}
-
-      {/* <Prop test="100" /> */}
-
-      {/* <Mapping /> */}
-
-      {/* <Usestate_hook/> */}
-
-      {/* <UseStateHook2/> */}
-
-      {/* <Usestatehook3/> */}
-
-      {/* <Event_handler/> */}
-
-      {/* <EventHandler2/> */}
-
-      {/* <EventHandle_onchange3/> */}
-
-      {/* <Practice1/> */}
-
-      {/* <Form_data_show/> */}
-
-      {/* <Practice1/> */}
-
-      {/* <Practice2/> */}
-
-      {/* <Practice3/> */}
-
-      {/* <Passing_data_child_to_parent/> */}
-
-      {/* <Child_to_parent/> */}
-
-      {/* <Parent3/> */}
-
-      {/* <Parent4/> */}
-
-      {/* <Parent5/> */}
-
-      {/* <State_lifting/> */}
-
-      {/* <Home/> */}
-
-      {/* <Practice2Home/> */}
-
-      {/* <Practice3_Home/> */}
-
       {/* <Todo_App/> */}
 
-      {/* <Toggle_Practice/> */}
-
-      {/* <Practice_toggle2/> */}
-
-      {/* <FAQ_Practice/> */}
-
-      {/* <Faq2_Practice/> */}
-
-      {/* <Faq3_practice/> */}
-
-      {/* <Useeffect_practice1/> */}
-
-      {/* <UseEffect2/> */}
-
-      {/* <UseEffect3_practice/> */}
-
-      {/* <UseEffect_Error_fetch/> */}
-
-      {/* <UseEffectErrorPrac2/> */}
-
-      {/* <Custom_hook1/> */}
-
-      {/* <Custom_Hook2/> */}
 
       <COuntry_App/>
 
-      {/* <Dynamic_Style_1/> */}
-
-      {/* <Dynamic_style2/> */}
-
-      {/* <Style3_dynamic/> */}
-
-      {/* <Component1/> */}
-
-      {/* <Usereducer/> */}
     </>
   );
 }
---
+++ src/Country2_App/Country2.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Country2 = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Country2
