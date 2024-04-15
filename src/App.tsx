6ea05a427a6218b12149f3ebfc557e4ee857d144
imran <zunaeede@gmail.com>
2024-04-15 22:07:21 +0600

    todo_tsx - making new practice - redux todo - typescript.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import './App.css'
-import Counter2 from './components/Counter2'
-// import Counter_App from './components/Counter_App'
 
 function App() {
 
 
   return (
     <>
-      {/* <Counter_App/> */}
-      <Counter2/>
+      
     </>
   )
 }
---
+++ src/components/Todo.tsx
@@ -0,0 +1,12 @@
+
+const Todo = () => {
+
+  return (
+
+    <>
+      
+    </>
+  )
+}
+
+export default Todo;
